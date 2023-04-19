d39a2b027e1327ea0af07afa56a2c706741f1256
Alex Urtubia Contreras <alexurtubia@pop-os.localdomain>
2023-04-19 00:18:49 -0400

    agregado divider en comparación

--- src/views/glaciers/Comparar.jsx
+++ src/views/glaciers/Comparar.jsx
@@ -3,11 +3,11 @@ import ReMap from '../../components/Map/ReMap'
 
 function Comparar() {
   return (
-    <div className="d-flex flex-row">
-      <div className='w-50 border-end border-2 border-dark '>
+    <div className="d-flex flex-row w-100">
+      <div className='w-50 border-end border-3 border-dark'>
         <ReMap/>
       </div>
-      <div className='w-50'>
+      <div className='w-50 border-3 border-dark'>
         <ReMap/>
       </div>
       {/* 
